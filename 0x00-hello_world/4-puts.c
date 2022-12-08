9fd253ff64c1f3e625ce5915ccbfd628fe2e1925
Tam <Thebeginning724@gmail.com>
2022-12-08 23:45:02 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	char text[] = "\"Programming is like building a multilingual puzzle\n";
+	char text[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(text);
 	return (0);
