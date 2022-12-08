7ede1b7bbbe46dd5b36cd816e4a9c922ed3af77a
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-08 23:39:50 +0100

    Added a script that prints with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *
+ *Description - Printing strings with put
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char text[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(text);
+	return (0);
+}
