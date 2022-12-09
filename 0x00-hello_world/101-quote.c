3c1246ed0b08107c09573c295929d605606dd249
Tam <Thebeginning724@gmail.com>
2022-12-09 01:42:37 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 60, 1, stderr);
+	fwrite("[ERROR] : and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 73, 1, stderr);
 	return (1);
 }
