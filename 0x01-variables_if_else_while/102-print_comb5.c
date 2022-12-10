09ecb6160ea4b7f951df8c69570fc8b7414db070
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-10 20:13:00 +0100

    Modified the 102-print_combo file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
+
 /**
-* main - Prints all possible combinations of a pair of
-* two digit numbers, without any repetition.
-*
-* Return: 0 on success.
-*/
+ *main - Entry Point
+ *
+ *Description - Program to output all possible combinations of a pair of two
+ *digits number, without any repetition
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-int i, j;
-for (i = 0; i <= 99; i++)
-{
-for (j = 0; j <= 99; j++)
-{
-if (i < j && i != j)
-{
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-putchar(' ');
-putchar((j / 10) + '0');
-putchar((j % 10) + '0');
-if (i != 98 || j != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int i = 0;
+	int j = 0;
+
+	while (i <= 99)
+	{
+		while (j <= 99)
+		{
+			if (i < j && i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98)
+					putchar(',');
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
