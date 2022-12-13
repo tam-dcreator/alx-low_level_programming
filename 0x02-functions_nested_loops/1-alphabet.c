d89eb27aba300c73b1f1831fa3ebd515fbe8769c
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-13 22:36:49 +0100

    Added a new function, modified header and gitignore

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Prints the alphabet in lower case
+ *
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
