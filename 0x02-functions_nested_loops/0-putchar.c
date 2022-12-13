854e9763992967fcee9a2ecbc5cddd9548e72dc6
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-13 14:55:32 +0100

    Added a new dir and a new source file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description - A program to output putchar
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return 0;
+}
