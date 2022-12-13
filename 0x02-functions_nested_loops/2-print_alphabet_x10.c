e97c03f59e749019b3e10a1b8e5e06302ad83b92
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-13 22:56:20 +0100

    Added a funtion and modified the header

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Prints 10 times the alphabet in lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
