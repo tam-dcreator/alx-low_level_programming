bced0cabafdbaf7f3fdad8858994fa23ef424788
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-14 00:36:27 +0100

    Added a new function and modified header

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *print_last_digit - Function to print last digit of a number
+ *@n: Integer to be evaluated
+ *
+ *Return: The last digit is returned
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
