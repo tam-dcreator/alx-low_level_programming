9c40b73048860ec3cfe3839fc9f45b2d6f98ceed
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-14 22:50:15 +0100

    Modified function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,34 +8,37 @@ void print_times_table(int n)
 {
 	int num = 0, multi, prod;
 
-	while (num <= n)
+	if (n < 15 && n > 0)
 	{
-		multi = 1;
-
-		_putchar('0');
-		while (multi <= n)
+		while (num <= n)
 		{
-			_putchar(',');
-			_putchar(' ');
-			prod = multi * num;
+			multi = 1;
 
-			if (prod <= 9)
+			_putchar('0');
+			while (multi <= n)
 			{
+				_putchar(',');
 				_putchar(' ');
+				prod = multi * num;
+
+				if (prod <= 9)
+				{
+					_putchar(' ');
+				}
+				else if (prod > 99)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10 % 10) + '0');
+				}
+				else
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+				++multi;
 			}
-			else if (prod > 99)
-			{
-				_putchar((prod / 100) + '0');
-				_putchar((prod / 10 % 10) + '0');
-			}
-			else
-			{
-				_putchar((prod / 10) + '0');
-			}
-			_putchar((prod % 10) + '0');
-			++multi;
+			++num;
+			_putchar('\n');
 		}
-		++num;
-		_putchar('\n');
 	}
 }
