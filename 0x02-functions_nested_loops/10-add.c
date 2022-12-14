c63231470453f8317ec831ea9f8cc63adf508344
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-14 15:39:03 +0100

    Added three new functions and modified the header file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - Function to add two integers
+ *@a: First integer
+ *@b: Second integer
+ *
+ *Return: Sum of the two integers
+ */
+int add(int a, int b)
+{
+	int sum = (a + b);
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *times_table - Function to list out the 9 times table
+ *
+ */
+void times_table(void)
+{
+	int i = 0, j, output;
+
+	while (i < 10)
+	{
+		j = 1;
+		output = 0;
+
+		_putchar('0');
+		while (j < 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			output = i * j;
+			if (output <= 9)
+				_putchar(' ');
+			else
+				_putchar((output /10) + '0');
+			_putchar((output % 10) + '0');
+			++j;
+		}
+		_putchar('\n');
+		++i;
+	}
+}
