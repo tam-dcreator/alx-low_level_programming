521642660ab7bed7cce52482916dd17ba5b04f5a
Tam <Thebeginning724@gmail.com>
2022-12-15 22:08:01 +0100

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0) && !(year % 100 == 0))
 	{
-		if (month >= 2 && day >= 61)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
@@ -22,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else if (year % 400 == 0)
 	{
-		if (month >= 2 && day >= 61)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
