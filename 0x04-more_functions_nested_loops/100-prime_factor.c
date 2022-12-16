198f0d5c6f9b4d06f1803aed3dc02d2556c18887
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-17 00:07:19 +0100

    Added function for Prime factor and modified header

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
+int prime_number(void);
+
+int main(void)
+{
+	int result;
+
+	result = prime_number();
+	printf("%d\n", result);
+
+	return (0);
+}
+
+/**
+ *prime_number - Function to display the highest prime number of a 61282475143
+ *
+ *Return: The highest prime number
+ */
+int prime_number(void)
+{
+	long int num = 61282475143;
+	int i, int_sqrt, count, ans;
+
+	for ( ; num > 0; num--)
+	{
+		int_sqrt = ceil(sqrt(num));
+		count = 0;
+
+		for (i = 2; i < int_sqrt + 1; i++)
+		{
+			if (num % i == 0)
+				count++;
+		}
+		if (count == 0)
+		{
+			ans = num;
+			break;
+		}
+	}
+	return (ans);
+}
