c2d3e97fd5dfc7c127b83101141dd9946fa35304
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-16 04:57:33 +0100

    Added the fizzbuzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 15 == 0)
+			printf("Fizzbuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
