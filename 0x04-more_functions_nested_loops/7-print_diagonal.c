92573948f2b10b961b761e956a8b348e4264002c
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-16 04:25:49 +0100

    Added a new function and modified header file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_diagonal - Function to display slashes n times
+ *@n: Number of times diagonal is displayed
+ *
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			int j = 1;
+
+			while (j++ <= i)
+				_putchar(' ');
+			_putchar('\\');
+			if (i < n)
+				_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
