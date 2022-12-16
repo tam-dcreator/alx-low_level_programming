f8f392925c650320d2c52fc23e4b9d54b6371188
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-16 03:00:15 +0100

    Added a function and modified header file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - Function to multiply two ints
+ *@a: First int
+ *@b: Second int
+ *
+ *Return: Multiplied value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
