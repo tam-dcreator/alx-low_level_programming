6fa78ed92f4cb1caaa55a16a378234cf5a4969a5
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-16 05:22:25 +0100

    Modified Fizzbuzz program

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  *main - Entry Point
@@ -9,20 +9,22 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; ++i)
+	for (i = 1; i <= 100; ++i)
 	{
-		if (i % 15 == 0)
-			printf("Fizzbuzz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else if (i % 3 == 0)
+		if (i % 3 == 0 && !(i % 5 == 0))
 			printf("Fizz");
+		else if (i % 5 == 0 && !(i % 3 == 0))
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
 		else
 			printf("%d", i);
+
 		if (i != 100)
 			printf(" ");
 		else
 			printf("\n");
 	}
+
 	return (0);
 }
