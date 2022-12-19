f923c0540e25eec9f0ea836d04500d4d586741d5
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-20 00:36:14 +0100

    Added function swap and modified header

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *swap_int - Function to swap the values of two integers
+ *@a: First pointer integer
+ *@b: Second pointer integer
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
