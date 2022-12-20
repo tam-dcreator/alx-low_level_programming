97c3e2f1e4e1f5b00674a42497402ddba8fecf6d
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-20 20:10:33 +0100

    Added function rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *rev_string - Function to reverse a string
+ *@s: String that would be reversed
+ *
+ */
+void rev_string(char *s)
+{
+	int str_len = 0, a = 0, i;
+	char *end, *start, temp;
+
+	while (*(s + a) != '\0')
+	{
+		str_len++;
+		a++;
+	}
+
+	*end = s;
+	*start = s;
+
+	for (i = 0; i < count - 1; i++)
+		end++;
+
+	for (i = 0; i < count / 2; i++)
+	{
+		temp = *end;
+		*end = *start;
+		*start = temp;
+
+		start++;
+		end--;
+	}
+}
