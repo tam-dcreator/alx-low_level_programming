8c7c471a933941b045cd82451bf13c4060aa445a
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-20 01:50:32 +0100

    Created a function strlen and modified header

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - Function to print the length of a string
+ *@s: String to be evaluated
+ *
+ *Return: Total length of string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	while (*s != '\0')
+	{
+		count++;
+		*s++;
+	}
+	return (count);
+}
