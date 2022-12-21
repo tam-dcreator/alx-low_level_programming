e9ee8337aa3b09be39fa9282d76d060f7e4110b8
tam-dcreator <tam@DESKTOP-EQKR7KI>
2022-12-21 02:05:09 +0100

    Added function puts half and modified header

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *puts_half - Function to print half a string
+ *@str: String to be evaluated
+ *
+ */
+void puts_half(char *str)
+{
+	int a = 0, len = 0, i, n;
+
+	while (*(str + a) != '\0')
+	{
+		len++;
+		a++;
+	}
+
+	n = len / 2;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (len % 2 == 0)
+			_putchar(str[n + i]);
+		else
+			_putchar(str[n + 1 + i]);
+	}
+	_putchar('\n');
+}
