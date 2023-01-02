125f1bfc7f72e1f80326d8ef4f0423fbfd86c5aa
Tam <Thebeginning724@gmail.com>
2023-01-02 20:23:22 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-        int sumA = 0, sumB = 0;
-
-        while ((*s1 != '\0') && (*s2 != '\0'))
+        while ((*s1 != '\0') && (*s2 != '\0') && (*s1 == *s2))
         {
-                sumA += *s1;
-                sumB += *s2;
                 s1++;
                 s2++;
         }
-        return (sumA - sumB);
+        return (*s1 - *s2);
 }
