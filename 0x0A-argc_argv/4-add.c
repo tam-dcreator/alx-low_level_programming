f4e970470fa2a0e2e42c373069d07f12b337f5de
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-07 00:07:09 +0100

    Added program add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *num_checker - checks if a given char is number or not
+ *@a: char to be checked
+ *
+ *Return: 1, if its a number, else 0
+ */
+int num_checker(char *a)
+{
+	int i, num, len;
+
+	i = 0;
+	num = 0;
+	len = strlen(a);
+	while (i < len)
+	{
+		if (a[i] < '0' || a[i] > '9')
+		{
+			return (-1);
+		}
+		else
+			num = num * 10 + (a[i] - '0');
+		i++;
+	}
+	return (num);
+}
+
+/**
+ *main - Entry Point
+ *@argc: Argument count
+ *@argv: Argument vector
+ *
+ *Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, num, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		num = num_checker(argv[i]);
+		if (num == -1)
+		{
+			printf("Error\n");
+			return (0);
+		}
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
