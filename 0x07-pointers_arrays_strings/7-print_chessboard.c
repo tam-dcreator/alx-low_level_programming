16578c2478904f7b1acb3142f4aee6299b022f03
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-09 14:01:22 +0100

    Added function print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_chessboard - Function that prints the chessboard
+ *@a: 2D Array
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i][7] != '\0'; i++)
+	{
+		for (j = 0; j < 8; j++)
+			putchar(a[i][j]);
+		putchar('\n');
+	}
+}
