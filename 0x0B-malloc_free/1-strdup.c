5937e2d4db4b04e5b4be9b6c5177b2b56ec13f30
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-10 19:40:26 +0100

    Added function strdup and modified header

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - Creates a copy of an input string in newly allocated memory
+ *@str: String thats copied
+ *
+ *Return: Pointer to duplicate string or null
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int len = 0, i = 0;
+
+	/*First get the length of the input*/
+	while (str[i++] != '\0')
+		len++;
+
+	if (len == 0)
+		return (NULL);
+
+	/*Create a malloc thats the same size as the input string*/
+	s = malloc(sizeof(char) * len);
+
+	if (s == NULL)
+		return (NULL);
+	/*Initialize the allocated space with values*/
+	i = 0;
+	while (i < len)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+
+	return (s);
+}
