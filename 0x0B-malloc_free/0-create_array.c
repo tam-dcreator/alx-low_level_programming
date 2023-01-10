00d1721df1390c1c001358d6c7c74a98c1a91931
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-10 15:37:08 +0100

    Modified create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,10 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i = 0;
 
-	if (size == 0)
-		return ('\0');
-
 	s = malloc(sizeof(char) * size);
+
+	if (s == '\0')
+		return ('\0');
 
 	while (i < size)
 	{
