071c882a5c77799433250c5cbb11c7c104da7327
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-10 15:31:19 +0100

    Added create_array function, readme and header file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *create_array - Function that creates an array of chars and initializes with a
+ *specific char
+ *@size: Size of the malloc to create
+ *@c: Specific char to initialize the array
+ *
+ *Return: Pointer to array if successful or NULL.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return ('\0');
+
+	s = malloc(sizeof(char) * size);
+
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Readme for C-malloc,free project
