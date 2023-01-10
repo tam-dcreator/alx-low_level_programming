981dcc5d1ad9f11250bbc98cca18323c58d94305
Tam <Thebeginning724@gmail.com>
2023-01-10 22:11:44 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	int len = 0, r = width, c = height - 1;
+	int len = 0, r = width, c = height;
 	int **grid, *ptr;
 
 	len = sizeof(int *) * r + sizeof(int) * c * r;
