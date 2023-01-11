70a0922c0929b92cda166ff8064ffb8bcca1f04f
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-11 04:35:00 +0100

    Modified alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,19 +12,31 @@ int **alloc_grid(int width, int height)
 {
 	int i, j;
 	int c = width, r = height;
-	int **grid = (int **)malloc(r * sizeof(int *));
+	int **grid;
 
-	if (c == 0 && r == 0)
+	if (c <= 0 || r <= 0)
 		return (NULL);
+
+	/*Reserve memory allocations for all rows*/
+	grid = malloc(r * sizeof(int *));
 
 	if (grid == NULL)
 		return (NULL);
 
+	/*For every row created, reserve memory allocations for*/
+	/*corresponding colomn*/
 	for (i = 0; i < r; i++)
 	{
 		grid[i] = (int *)malloc(c * sizeof(int));
+		/*If any of the memory allocation fails, remove all*/
 		if (grid[i] == NULL)
+		{
+			for (i = 0; i < r; i++)
+				free(grid[i]);
+			free(grid);
+
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < r; i++)
