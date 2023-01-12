093cface40a5f5cb37455652254400cf91ebbfb3
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-12 22:05:19 +0100

    Modified string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,37 +12,39 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len = 0;
-	char *alloc, *temp;
+	unsigned int i, j, len, len1, len2;
+	char *s;
 
 	/*Check if string is null*/
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	/*Get length of strings s1 and s2*/
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 	/*Check if n is greater than s2*/
-	if (n >= strlen(s2))
-		len = strlen(s2) + 1;
+	if (n >= len2)
+		len2 = strlen(s2) + 1;
 	else
-		len = n;
-	/*Create memory allocation*/
-	alloc = malloc(sizeof(*alloc) * (len * strlen(s1)));
-	if (alloc == NULL)
-		return (NULL);
-	temp = alloc;
-	/*Initialise alloc with first string */
-	for (i = 0; i < strlen(s1); i++)
-	{
-		*temp = s1[i];
-		temp++;
-	}
-	/*Initialise alloc with second string*/
-	for (j = 0; j < len; j++)
-	{
-		*temp = s2[j];
-		temp++;
-	}
-	*temp = '\0';
+		len2 = n;
 
-	return (alloc);
+	len = len1 + len2;
+	/*Create memory allocation*/
+	s = malloc(sizeof(*s) * len);
+	if (s == NULL)
+		return (NULL);
+	/*Initialise alloc with first string */
+	i = 0, j = 0;
+	while (i < len)
+	{
+		if (i < len1 + 1)
+			s[i] = s1[i];
+		else
+			s[i] = s2[j++];
+		i++;
+	}
+	s[i] = '\0';
+
+	return (s);
 }
