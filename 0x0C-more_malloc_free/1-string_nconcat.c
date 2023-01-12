5bbe0b55f78129274ebcdfe4419942371fd7bf7e
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-12 22:45:25 +0100

    Another modification on string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len = (len1 + len2);
 	/*Create memory allocation*/
-	s = malloc(sizeof(*s) * len);
+	s = malloc(sizeof(*s) * len + 1);
 	if (s == NULL)
 		return (NULL);
 	/*Initialise s with values */
