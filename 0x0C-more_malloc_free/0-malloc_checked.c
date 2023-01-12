2fbe360bd11b782f2b1107476cc4901ea9aeae14
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-12 20:38:20 +0100

    Added malloc_checked function, header and readme file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checked - Function that allocates memory using malloc
+ *@b: Unsigned int for size of malloc
+ *
+ *Return: Pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *alloc = malloc(sizeof(*alloc) * b);
+
+	if (alloc == NULL)
+		exit(98);
+
+	return (alloc);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Readme for C-More malloc, free
