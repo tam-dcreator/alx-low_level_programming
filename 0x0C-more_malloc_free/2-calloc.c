06b7273deb6175b1c63eb4bb00da6da573f25350
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-13 04:42:46 +0100

    Modified calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,16 +13,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *s;
 
-	if (nmemb == 0)
-		return (NULL);
-	if (size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	s = malloc(size * nmemb);
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
-		s[i] = '0';
+	for (i = 0; i < nmemb * size; i++)
+		s[i] = 0;
+
 	return (s);
 }
