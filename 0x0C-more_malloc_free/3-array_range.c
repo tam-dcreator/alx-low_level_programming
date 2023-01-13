c06ec2a9473071e699a198f2d5c587f58d487996
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-13 05:06:24 +0100

    Modified function array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,11 +16,11 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	s = malloc(sizeof(*s) * (max - min) + 1);
+	s = malloc(sizeof(*s) * (max - min + 1));
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < max + 1; i++)
+	for (i = 0; i <= max; i++)
 	{
 		s[i] = min;
 		min++;
