ae41ce4180a46f9042067d21c580eed2178264d5
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-13 05:00:29 +0100

    Added function array_range and modified header

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *array_range - Function that creates an array of integers
+ *@min: Minimum integer
+ *@max: Maximun integer
+ *
+ *Return: Pointer to array or null
+ */
+int *array_range(int min, int max)
+{
+	int i;
+	int *s;
+
+	if (min > max)
+		return (NULL);
+
+	s = malloc(sizeof(*s) * (max - min + 1));
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < max + 1; i++)
+	{
+		s[i] = min;
+		min++;
+	}
+	return (s);
+}
