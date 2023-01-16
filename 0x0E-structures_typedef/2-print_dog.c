0f6eddfe8eb6fd4fbbdbd864e66badf1f2314942
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-17 00:42:23 +0100

    Modified print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,10 +16,13 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	printf("Age: %f\n", d->age);
+	if (d->age != d->age)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
