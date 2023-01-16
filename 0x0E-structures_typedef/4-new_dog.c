8d9ef4c409312e8056c8a4a664efbfebd2e84be1
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-17 00:59:27 +0100

    Added function new_dog and modified header

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *new_dog - Function that creates a new dog
+ *@name: Name of the dog
+ *@age: age of the dog
+ *@owner: Owner of the dog
+ *
+ *Return: Pointer to new struct of type dog_t or NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+
+	return (dog);
+}
