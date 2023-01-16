bef4537fe067217d0b2e1c4598a23f75484662c5
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-16 23:51:56 +0100

    Added function init_dog and modified header dog.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ *init_dog - Function that initialize a variable of type struct dog
+ *@d: Pointer to struct my_dog address
+ *@name: Name to pass to d
+ *@age: Age to pass to d
+ *@owner: to pass to d
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
