761892550992b0a2495f81806a3303dac5cfcdef
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-16 23:38:18 +0100

    Added a new dir, readme and struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ *struct dog - new type for dog info
+ *@name: Dogs name
+ *@age: Dogs age
+ *@owner: Dogs owner
+ *
+ *Description: Nothing else to add
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif/*HEADER_FILE*/
