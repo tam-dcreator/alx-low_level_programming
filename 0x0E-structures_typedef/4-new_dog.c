0c247a5af7de00c22d16d115c439f98c68dedbce
Tam <Thebeginning724@gmail.com>
2023-01-17 01:40:49 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -38,7 +38,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	/*Check if owner is null and free all malloc if it is*/
 	if (owner == NULL)
 	{
-		free(dog->owner);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
