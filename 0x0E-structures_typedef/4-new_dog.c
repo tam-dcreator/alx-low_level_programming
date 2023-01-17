c4c2481bbe4706cfb84279110c68c131ea5914a1
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-17 01:20:34 +0100

    Modified new_dog function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,18 +11,44 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	/*Create new dog from dog_t*/
 	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
+
 	if (dog == NULL)
+		return (NULL);
+
+	/*Check if name is NULL and free all malloc if it is*/
+	if (name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
 
+	/*Create a malloc for new name and copy name into it*/
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (dog->name == NULL)
+		return (NULL);
+	strcpy(dog->name, name);
+
+	dog->age = age;
+	/*Check if owner is null and free all malloc if it is*/
+	if (owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	/*Create a malloc for new owner and copy the value of owner into it*/
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+
+	if (dog->owner == NULL)
+		return (NULL);
+	strcpy(dog->owner, owner);
+
+	/*Return new dog*/
 	return (dog);
 }
