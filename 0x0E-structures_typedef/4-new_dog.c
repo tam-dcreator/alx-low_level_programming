0d92c4a986b00e24deadaa40f057293adb32de25
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-17 10:27:27 +0100

    Modified new_dog to make it betty compliant

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,7 +7,6 @@
  *@name: Name of the dog
  *@age: age of the dog
  *@owner: Owner of the dog
- *
  *Return: Pointer to new struct of type dog_t or NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
@@ -16,18 +15,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
-
 	if (dog == NULL)
 		return (NULL);
-
 	/*Check if name is NULL and free all malloc if it is*/
 	if (name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-
-
 	/*Create a malloc for new name and copy name into it*/
 	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (dog->name == NULL)
@@ -36,7 +31,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	strcpy(dog->name, name);
-
 	dog->age = age;
 	/*Check if owner is null and free all malloc if it is*/
 	if (owner == NULL)
@@ -45,10 +39,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-
 	/*Create a malloc for new owner and copy the value of owner into it*/
 	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
@@ -56,7 +48,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	strcpy(dog->owner, owner);
-
 	/*Return new dog*/
 	return (dog);
 }
