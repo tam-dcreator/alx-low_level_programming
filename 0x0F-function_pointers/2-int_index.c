7a3f43a20d7624098090390c4163c8cddba923ec
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-20 05:03:47 +0100

    Modified int_index and added op_functions

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (cmp != NULL && array != NULL)
 	{
-		for (i = 0; i < size;)
+		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
 				return (i);
-			i++;
 		}
 	}
 
