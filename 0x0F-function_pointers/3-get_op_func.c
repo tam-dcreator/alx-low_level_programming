a39a1b66c68110fdc69cd55b86fe55bf463c5c4e
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-23 19:38:21 +0100

    Revert "Optimised code"
    This reverts commit c5d614fda2640ba3aa4301ced4b173270bdf7e93.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,15 +16,15 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
+		{"NULL", NULL},
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (i < 6)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
+			return ((ops[i].f));
 		i++;
 	}
-	return (ops[i].f);
+	return (NULL);
 }
