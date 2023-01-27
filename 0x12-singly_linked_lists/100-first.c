39bc810f912b08872ffdc3d419119eb6fa39f6a4
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-01-27 01:25:20 +0100

    Added 100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ *before_main - Function that prints before main is executed.
+ *
+ *Return: Nothing.
+ */
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
