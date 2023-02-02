8da128cc4ad91dbf21a71da1f86bb4f5e793bc88
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-02-02 10:05:35 +0100

    Modified free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		temp = *head;
-		temp = temp->next;
+		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
