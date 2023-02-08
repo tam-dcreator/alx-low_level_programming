ac1098cfa524b98cc5136a92208672b52c7ecf2e
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-02-08 20:39:34 +0100

    Modified code in prog create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  *create_file - Function that creates a file
  *@filename: Name of file to be created
- *@text_contents: Contents to be added to file
+ *@text_content: Contents to be added to file
  *
  *Return: 1 on success, -1 on failure
  */
@@ -13,12 +13,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd < 0)
 		return (-1);
-	if (write(fd, text_content, strlen(text_content)) < 0)
-		return (-1);
+	if (text_content != NULL)
+	{
+		if (write(fd, text_content, strlen(text_content)) < 0)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
