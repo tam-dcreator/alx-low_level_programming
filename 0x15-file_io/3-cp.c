35b56d3028bad00aadea3d197cc5a19460284f7e
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-02-10 23:36:19 +0100

    Modified program code in cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fdt = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	fdt = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fdt < 0)
 		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 
