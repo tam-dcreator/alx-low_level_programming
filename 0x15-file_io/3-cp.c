2627f99fb0df721e831df1ccba2624b7f2353020
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-02-10 23:05:06 +0100

    Added code to prog cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ *main - Entry point for code that copies from one file to another
+ *@argc: Argument counter
+ *@argv: Pointer array to stdin characters
+ *
+ *Return: On error 1, 97, 98, 99, 100, on success (0).
+ */
+int main(int argc, char *argv[])
+{
+	int fdf, fdt, count, read_count, buf_size = 1024;
+	char *buf;
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+	fdf = open(argv[1], O_RDONLY);
+	if (fdf < 0)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fdt = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	if (fdt < 0)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	buf = calloc(sizeof(buf), buf_size);
+	if (buf == NULL)
+		return (1);
+	read_count = read(fdf, buf, buf_size);
+	if (read_count < 0)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	count = write(fdt, buf, read_count);
+	if (count < 0)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	close(fdf);
+	close(fdt);
+	free(buf);
+	return (0);
+}
