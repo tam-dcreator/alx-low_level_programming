272fea61feb0ff00f683c6e7ec11a0862d5e80f2
tam-dcreator <tam@DESKTOP-EQKR7KI>
2023-02-10 23:25:21 +0100

    Modified program code in cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,11 +13,10 @@ int main(int argc, char *argv[])
 	char *buf;
 
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
-	}
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]), exit(97);
+
 	fdf = open(argv[1], O_RDONLY);
+
 	if (fdf < 0)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
@@ -25,27 +24,32 @@ int main(int argc, char *argv[])
 	}
 	fdt = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (fdt < 0)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+
 	buf = calloc(sizeof(buf), buf_size);
 	if (buf == NULL)
 		return (1);
-	read_count = read(fdf, buf, buf_size);
+	while ((read_count = read(fdf, buf, buf_size)) > 0)
+	{
+		count = write(fdt, buf, read_count);
+
+		if (count < 0)
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+
+	free(buf);
+
 	if (read_count < 0)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	count = write(fdt, buf, read_count);
-	if (count < 0)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	close(fdf);
-	close(fdt);
-	free(buf);
+
+	if (close(fdf) != 0)
+		dprintf(2, "Error: Can't close fd %d\n", fdf), exit(100);
+
+	if (close(fdt) != 0)
+		dprintf(2, "Error: Can't close fd %d\n", fdt), exit(100);
+
 	return (0);
 }
