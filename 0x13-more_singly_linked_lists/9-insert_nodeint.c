7e62cf5cf1f681038c6ccd0876b48f49c8b8b4d5
tam-dcreator <tam@THE-7TH>
2023-03-19 14:51:31 +0100

    Modified the function to accomodate more case points

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,19 +13,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *newnode, *temp = *head;
 
-	if (*head == NULL)
-		return (NULL);
-
-	while (i++ < (idx - 1))
-		temp = temp->next;
-
 	newnode = malloc(sizeof(newnode));
 	if (newnode == NULL)
 		return (NULL);
 
 	newnode->n = n;
-	newnode->next = temp->next;
-	temp->next = newnode;
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
+	else
+	{
+		while (i++ < (idx - 1))
+		{
+			temp = temp->next;
+			if (temp == NULL)
+				return (NULL);
+		}
 
+		newnode->next = temp->next;
+		temp->next = newnode;
+	}
 	return (newnode);
 }
