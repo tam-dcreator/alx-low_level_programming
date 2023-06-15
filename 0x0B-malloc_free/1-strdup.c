ee062529cbcc7dac5f79e3059d75cb58fd7267f2
tam-dcreator <tam@THE-7TH>
2023-06-15 18:16:17 +0100

    Trimmed off excesses

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,14 +15,9 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-<<<<<<< HEAD
-	/* Use the sizeof operator to get the length of str */
-	len = sizeof(str);
-=======
 	/* Get the length of str */
 	while (str[i++])
 		len++;
->>>>>>> 6760bfb (Changed the way the length of the string to duplicate was calculated)
 
 	s = malloc(sizeof(*s) * len);
 
