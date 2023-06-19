578c82c11378c789a7cc3b4853ab39843621d1d5
tam-dcreator <tam@THE-7TH>
2023-06-19 11:56:27 +0100

    Modified function code

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,18 +8,17 @@
 */
 char *argstostr(int ac, char **av)
 {
-	char *str, *str_temp;
-	int i, j, len = (1 + ac), m = 0;
+	char *str;
+	int i, j, len = ac, m = 0;
 
-	if (ac == 0 || av == NULL)
+	if (ac <= 0 || av == NULL)
 		return (NULL);
 
 	/*Find the combined length of the individual strings in argv*/
-	for (j = 1; j < ac;)
+	for (j = 1; j < ac; j++)
 	{
-		str_temp = av[j++];
 		i = 0;
-		while (str_temp[i++] != '\0')
+		while (av[j][i++] != '\0')
 			len++;
 		/*Incrementing len again for new line char included per word */
 		len++;
@@ -28,12 +27,11 @@ char *argstostr(int ac, char **av)
 
 	if (str == NULL)
 		return (NULL);
-	for (j = 1; j < ac;)
+	for (j = 1; j < ac; j++)
 	{
-		str_temp = av[j++];
 		i = 0;
-		while (str_temp[i] != '\0')
-			str[m++] = str_temp[i++];
+		while (av[j][i] != '\0')
+			str[m++] = av[j][i++];
 		str[m++] = '\n';
 	}
 	str[m] = '\0';
