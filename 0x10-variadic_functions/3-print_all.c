846cee70b7162270697d430a633e991f5fa7e9ad
tam-dcreator <tam@THE-7TH>
2023-06-23 05:26:34 +0100

    Added function code that prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ *print_all - Function that prints strings
+ *@format: List of arguments to be passed
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i;
+	float f;
+	char const *str, *s;
+	va_list args;
+
+	str = format;
+	va_start(args, format);
+	while (str != NULL && *str != '\0')
+	{
+		switch (*str)
+		{
+		case 'c':
+			i = va_arg(args, int);
+			putchar(i);
+			break;
+		case 's':
+			s = va_arg(args, char*);
+			if (s == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", s);
+			break;
+		case 'i':
+			i = va_arg(args, int);
+			printf("%d", i);
+			break;
+		case 'f':
+			f = va_arg(args, double);
+			printf("%f", f);
+			break;
+		}
+		if (*(str + 1) != '\0' &&
+		    (*str == 's' || *str == 'c' || *str == 'i' || *str == 'f'))
+			printf(", ");
+		str++;
+	}
+	va_end(args);
+	printf("\n");
+}
