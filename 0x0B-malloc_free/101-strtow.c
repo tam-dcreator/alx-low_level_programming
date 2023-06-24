cc2c49b3d04f64aa2702ca57d8b6f31457928ec7
tam-dcreator <tam@THE-7TH>
2023-06-24 09:50:13 +0100

    Added function code to split a string into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <string.h>
+/**
+*strtow - Function that splits a string into words.
+*@str: The string to be spilted
+*
+*Return: Pointer to an array of strings or NULL
+*/
+char **strtow(char *str)
+{
+	char **ptr;
+	char *token;
+	char *copy = strdup(str);
+	int len = 0, i = 0, j = 0;
+	const char *delimiters = " ";
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			len++;
+		i++;
+	}
+
+	ptr = malloc(sizeof(char*) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+
+	token = strtok(copy, delimiters);
+
+	while (token)
+	{
+		ptr[j] = malloc(sizeof(char) * (strlen(token) + 1));
+		if (ptr[j] == NULL)
+			return (NULL);
+		strcpy(ptr[j], token);
+		j++;
+		token = strtok(NULL, delimiters);
+	}
+
+	return (ptr);
+}
