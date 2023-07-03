421071bcef70ddb06e22243fa28c034f0ba10621
tam-dcreator <tam@THE-7TH>
2023-07-03 19:09:28 +0100

    Function that runs before the main.c file in a program

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+*before_main - Function that runs before the main function in C
+*
+*/
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
