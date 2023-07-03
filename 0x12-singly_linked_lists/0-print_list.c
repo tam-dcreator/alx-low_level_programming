8d2899ab43edb4e3e6bc1a3b9b7f9cf05624ab3d
tam-dcreator <tam@THE-7TH>
2023-07-03 22:34:12 +0100

    More modification to pass more requirements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,16 @@
 */
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 	const list_t *head = h;
 
 	while (head)
 	{
-		count++;
 		if (head->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", head->len, head->str);
+		count++;
 		head = head->next;
 	}
 
