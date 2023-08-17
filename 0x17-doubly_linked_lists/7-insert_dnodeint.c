797227886e732fbc88f86dee2651141013329875
tam-dcreator <tam@THE-7TH>
2023-08-17 16:24:19 +0100

    Created a function that inserts a new node at a given position.

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ *insert_dnodeint_at_index - Function that inserts a new node at a position
+ *@h: Pointer to the head of the list
+ *@idx: Index where list would be inserted
+ *@n: Data to be inserted
+ *
+ *Return: Address to new node or null
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+}
