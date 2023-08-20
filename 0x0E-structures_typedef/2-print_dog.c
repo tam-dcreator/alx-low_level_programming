a55ea87eb7a68d4dbcc3553eb7345a515aed59f4
tam-dcreator <tam@THE-7TH>
2023-08-20 09:23:17 +0100

    Remodelled code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,34 +1,28 @@
 #include "dog.h"
+#include <stdio.h>
+
 /**
-*print_dog - Function that prints a struct dog
-*@d: Pointer to a struct dog
-*
-*/
+ *print_dog - Function that prints dog info from struct dog
+ *@d: Struct containing dog info
+ *
+ */
 void print_dog(struct dog *d)
 {
-	char *c = "(nil)";
+	if (d == NULL)
+		return;
 
-	if (d)
-	{
-		if (d->name && d->age && d->owner)
-		{
-			printf("Name: %s\nAge: %f\nOwner: %s\n",
-			       d->name, d->age, d->owner);
-		}
-		else if (!d->name)
-		{
-			printf("Name: %s\nAge: %f\nOwner: %s\n",
-			       c, d->age, d->owner);
-		}
-		else if (!d->age)
-		{
-			printf("Name: %s\nAge: %s\nOwner: %s\n",
-			       d->name, c, d->owner);
-		}
-		else if (!d->owner)
-		{
-			printf("Name: %s\nAge: %f\nOwner: %s\n",
-			       d->name, d->age, c);
-		}
-	}
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age != d->age)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
