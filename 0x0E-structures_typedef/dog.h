b71b284d2a865ec7e1ef8421c7c616c154cc6e7d
tam-dcreator <tam@THE-7TH>
2023-08-20 08:21:20 +0100

    Defined a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+*struct dog - A structured dog
+*@name: Name of dog
+*@age: Age of dog
+*@owner: Owner of Dog
+*
+*Description: A dog structure
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
