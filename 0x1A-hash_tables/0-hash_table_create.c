367d414d5e40250ae3cfd86da9ad294fe3b426b8
tam-dcreator <tam@THE-7TH>
2023-09-08 21:30:41 +0100

    Created a function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,17 +8,31 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *array = NULL;
+	hash_table_t *array_ptr = NULL;
+	unsigned int index;
 
 	if (size == 0)
 		return (NULL);
 
-	array = malloc(sizeof(hash_table_t) * size);
+	array_ptr = malloc(sizeof(hash_table_t));
 
-	if (array == NULL)
+	if (array_ptr == NULL)
 	{
 		fprintf(stderr, "Malloc Failed in hash_table_create.\n");
 		return (NULL);
 	}
-	return (array);
+
+	array_ptr->array = malloc(sizeof(hash_node_t) * size);
+
+	if (array_ptr->array == NULL)
+	{
+		free(array_ptr);
+		return (NULL);
+	}
+	array_ptr->size = size;
+
+	for (index = 0; index < size; index++)
+		array_ptr->array[index] = NULL;
+
+	return (array_ptr);
 }
