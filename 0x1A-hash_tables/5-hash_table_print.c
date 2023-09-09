a520da4e133c811902a188292cc5bf905fa8d4e1
tam-dcreator <tam@THE-7TH>
2023-09-09 01:24:39 +0100

    Created a function that prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+*hash_table_print - Funxtion that prints a hash table
+*
+*@ht: The hash table
+*
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	long unsigned int i;
+	int flag = 1;
+
+	printf("{");
+	if (ht != NULL)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			temp = ht->array[i];
+			while (temp)
+			{
+				if (flag)
+					printf("'%s': '%s'", temp->key, temp->value);
+				else
+					printf(", '%s': '%s'", temp->key, temp->value);
+				temp = temp->next;
+				flag = 0;
+			}
+		}
+	}
+	printf("}\n");
+}
