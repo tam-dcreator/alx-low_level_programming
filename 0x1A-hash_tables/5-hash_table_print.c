2803419f7c76012895756263ab88c1e96c13f145
tam-dcreator <tam@THE-7TH>
2023-09-09 01:29:19 +0100

    Modified code to account for null and some c standard errors

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,24 +9,23 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *temp;
-	long unsigned int i;
+	unsigned long int i;
 	int flag = 1;
 
+	if (ht == NULL)
+		return;
 	printf("{");
-	if (ht != NULL)
+	for (i = 0; i < ht->size; i++)
 	{
-		for (i = 0; i < ht->size; i++)
+		temp = ht->array[i];
+		while (temp)
 		{
-			temp = ht->array[i];
-			while (temp)
-			{
-				if (flag)
-					printf("'%s': '%s'", temp->key, temp->value);
-				else
-					printf(", '%s': '%s'", temp->key, temp->value);
-				temp = temp->next;
-				flag = 0;
-			}
+			if (flag)
+				printf("'%s': '%s'", temp->key, temp->value);
+			else
+				printf(", '%s': '%s'", temp->key, temp->value);
+			temp = temp->next;
+			flag = 0;
 		}
 	}
 	printf("}\n");
