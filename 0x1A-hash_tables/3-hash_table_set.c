086c6b5773229691cd2fa0d12c243ac6dda1277c
tam-dcreator <tam@THE-7TH>
2023-09-10 08:58:06 +0100

    Modified the function to properly allow memory management and synergy with other functions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,43 +11,44 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newnode;
+	hash_node_t *newnode, *temp;
 	unsigned long int index;
 
 	if (key == NULL || ht == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+/*Check if entry is a duplicate before creating a new entry*/
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
 
 	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
 		return (0);
-
-/*Allocate memory for key and value strings*/
-	newnode->key = malloc(strlen(key) + 1); /* +1 for the null terminator*/
-	newnode->value = malloc(strlen(value) + 1); /* +1 for the null*/
-
+/*Copy the key and value into the newnode*/
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
 	if (newnode->key == NULL || newnode->value == NULL)
 	{
 		free_item(newnode);
 		return (0);
 	}
-
-	strcpy(newnode->key, key);
-	strcpy(newnode->value, value);
 	newnode->next = NULL;
-
 	if (ht->array[index] == NULL)
 		ht->array[index] = newnode;
 	else
 	{
-		if (strcmp(ht->array[index]->key, key) == 0)
-			strcpy(ht->array[index]->value, value);
-		else
-		{
-			newnode->next = ht->array[index];
-			ht->array[index] = newnode;
-		}
+		newnode->next = ht->array[index];
+		ht->array[index] = newnode;
 	}
 	return (1);
 }
