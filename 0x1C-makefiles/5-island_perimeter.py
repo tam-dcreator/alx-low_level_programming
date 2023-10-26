35daac71fdc10b6a95160215afbf4708d96b2043
tam-dcreator <tam@THE-7TH>
2023-10-26 15:52:51 +0100

    Technical Interview preparation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,18 +8,30 @@ def island_perimeter(grid):
     """ A function that returns the perimeter of the island described in grid
 
     :arg grid:
-        (list) A list of list of integers
+        (list) A nested list of integers
     :return:
         int: The perimeter of the island
     """
     if not isinstance(grid, list):
         return
 
-    length = 0
-    breadth = 1
-    for g_list in grid:
-        if 1 in g_list:
-            length += 1
-        if g_list.count(1) > 1:
-            breadth = g_list.count(1)
-    return (length + breadth) * 2
+    perimeter = 0
+    # Iterate through the length
+    for i in range(len(grid)):
+        # Iterate through the breadth
+        for j in range(len(grid[i])):
+            # If you encounter 1, perimeter of that space becomes 4
+            if grid[i][j] == 1:
+                perimeter += 4
+                # Check if the space is bothered by other valid spaces
+                # Reduce perimeter if it is.
+                if i != 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i != len(grid) - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j != 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j != len(grid[i]) - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
