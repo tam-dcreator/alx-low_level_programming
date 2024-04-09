a878078cf883f44fea0e661ca8379db421ab545c
tam-dcreator <tam@THE-7TH>
2024-04-09 15:20:07 +0100

    Function that searches for a value in an array of integers using binary search algo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,8 +1,8 @@
 #include "search_algos.h"
 
 /**
-*linear_search - Function that searches for a value in an array of integers
-*using linear search
+*binary_search - Function that searches for a value in an array of integers
+*using binary search algorithm
 *
 *@array: Pointer to the first element of the array to search in
 *@size: Number of elements in the array
@@ -10,19 +10,48 @@
 *
 *Return: Integer index of search item or -1
 */
-int linear_search(int *array, size_t size, int value)
+int binary_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	size_t i = 0, j = size - 1, mid;
 
 	if (size == 0 || !array)
 		return (-1);
 
-	while (i < size)
+	while (i <= j)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		mid = (i + j) / 2;
+		print_array(array, i, j);
+
 		if (array[i] == value)
-			return (i);
-		i++;
+		{
+			return (mid);
+		}
+		else
+		{
+			if (array[mid] > value)
+				j = mid - 1;
+			else
+				i = mid + 1;
+		}
 	}
 	return (-1);
+}
+
+/**
+*print_array - Function that prints array per iteration
+*
+*@array: Pointer to the first element of the array to search in
+*@start_index: Starting index
+*@j: Final index
+*
+*/
+void print_array(int *array, int start_index, int j)
+{
+	int i;
+
+	printf("Searching in array:");
+	for (i = start_index; i < j; i++)
+		printf(" %d,", array[i]);
+
+	printf(" %d\n", array[j]);
 }
