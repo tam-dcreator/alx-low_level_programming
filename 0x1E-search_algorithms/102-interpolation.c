964b89005796cc1500eee4578045054b7f039930
tam-dcreator <tam@THE-7TH>
2024-04-10 17:29:31 +0100

    Function that searches for a value in an array of integers using interpolation search algo

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+*interpolation_search - Function that searches for a value in an array of
+*integers using interpolation search algorithm
+*
+*@array: Pointer to the first element of the array to search in
+*@size: Number of elements in the array
+*@value: Value to search for
+*
+*Return: Integer index of search item or -1
+*/
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low = 0, high = size - 1, pos;
+
+	/*Check if array is empty or null*/
+	if (size == 0 || !array)
+		return (-1);
+
+	/*Determine probe position*/
+	pos = low + (((double)(high - low) /
+		      (array[high] - array[low])) * (value - array[low]));
+
+	while (pos < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] > value)
+			high = pos - 1;
+		else
+			low = pos;
+		pos = low + (((double)(high - low) /
+		      (array[high] - array[low])) * (value - array[low]));
+	}
+	return (-1);
+}
