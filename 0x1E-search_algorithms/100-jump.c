f0c33a6396800eb2624635972ecb826a1dd34bf1
tam-dcreator <tam@THE-7TH>
2024-04-10 15:58:17 +0100

    Function that searches for a value in an array of integers using jump search algo

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+*jump_search - Function that searches for a value in an array of integers
+*using jump search algorithm
+*
+*@array: Pointer to the first element of the array to search in
+*@size: Number of elements in the array
+*@value: Value to search for
+*
+*Return: Integer index of search item or -1
+*/
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i = 0, jmp_size = sqrt(size), low;
+
+	if (size == 0 || !array)
+		return (-1);
+
+	while (i < size)
+	{
+		if (array[i] < value)
+			printf("Value checked array[%ld] = [%d]\n"
+			       , i, array[i]);
+
+		else if (array[i] >= value)
+		{
+			low = i - jmp_size;
+			printf("Value found between indexes [%ld] and [%ld]\n"
+			       , low, i);
+			while (low <= i)
+			{
+				printf("Value checked array[%ld] = [%d]\n"
+				       , low, array[low]);
+				if (array[low] == value)
+					return (low);
+				low++;
+			}
+		}
+		i += jmp_size;
+	}
+	return (-1);
+}
